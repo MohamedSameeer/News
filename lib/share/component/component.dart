ca9e4308273413b84ab298192132b5b910207315
MohamedSameeer <mohamedsameeer96@gmail.com>
2021-04-29 04:14:19 +0200

    Web view

--- lib/share/component/component.dart
+++ lib/share/component/component.dart
@@ -1,50 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:news/modules/web_view/web_view.dart';
 
 Widget itemList({
   @required List articles,
   @required BuildContext context,
 }) {
   return ListView.separated(
-    itemBuilder: (BuildContext context, int index) => Container(
-      padding: EdgeInsets.all(16.0),
-      child: Row(
-        children: [
-          Container(
-            width: 120,
-            height: 140,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              image: DecorationImage(
-                  image: NetworkImage(
-                    articles[index]["urlToImage"] != null
-                        ? articles[index]["urlToImage"]
-                        : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png",
-                  ),
-                  fit: BoxFit.cover),
+    itemBuilder: (BuildContext context, int index) => InkWell(
+      onTap:() {navigateTO(context, WebViewScreen(articles[index]['url']));},
+      child: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Container(
+              width: 120,
+              height: 140,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: DecorationImage(
+                    image: NetworkImage(
+                      articles[index]["urlToImage"] != null
+                          ? articles[index]["urlToImage"]
+                          : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png",
+                    ),
+                    fit: BoxFit.cover),
+              ),
             ),
-          ),
-          SizedBox(
-            width: 16.0,
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  articles[index]["title"],
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-                Text(
-                  articles[index]["publishedAt"],
-                  style: TextStyle(
-                    color: Colors.grey,
-                  ),
-                ),
-              ],
+            SizedBox(
+              width: 16.0,
             ),
-          ),
-        ],
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    articles[index]["title"],
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                  Text(
+                    articles[index]["publishedAt"],
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     ),
     separatorBuilder: (BuildContext context, int index) => Container(
